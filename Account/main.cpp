09fbe4427d3b7b62d5a5779790cff8d2b5fa5602
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2018-09-01 13:20:29 +0200

    Test sql

--- Account/main.cpp
+++ Account/main.cpp
@@ -1,6 +1,37 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QDebug>
+#include <QRandomGenerator>
+#include "Controller/controllerdb.h"
+
+void test()
+{
+    ControllerDB cdbb;
+    qDebug()<<"Test";
+    auto accounts = cdbb.selectAccount();
+    qDebug()<<accounts;
+    auto entries1 = cdbb.selectEntry(accounts[0]);
+    qDebug()<<entries1.size()<<accounts[0];
+    for(auto e: entries1)
+        qDebug()<<e.id()<<e.value()<<e.date()<<e.type();
+
+    Entry e;
+    QRandomGenerator rdn;
+    rdn.bounded(999.9);
+    e.setAccount(accounts[0]);
+    e.setValue(rdn.generateDouble());
+    e.setDate(QDate::currentDate());
+    e.setType("outcome");
+    cdbb.addEntry(e);
+
+    auto entries2 = cdbb.selectEntry(accounts[0]);
+    qDebug()<<entries2.size()<<accounts[0];
+    for(auto e: entries2)
+        qDebug()<<e.id()<<e.value()<<e.date()<<e.type();
+
+
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -15,5 +46,9 @@ int main(int argc, char *argv[])
         return -1;
 
     qDebug()<<engine.rootObjects();
+
+    if(argc > 1 && QString(argv[1]) == "--test")
+        test();
+
     return app.exec();
 }
