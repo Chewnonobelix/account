1268b347fab798561ddabc8411c4bcc24f2f64e7
Arnaud DUHAMEL <a.duhamel@ateme.com>
2018-12-17 15:01:57 +0100

    [XmlToSql]

--- Account/Controller/xmltosql.cpp
+++ Account/Controller/xmltosql.cpp
@@ -1,6 +1,62 @@
 #include "xmltosql.h"
 
-XmltoSql::XmltoSql()
+XmltoSql::XmltoSql(ControllerXML& xml): m_xml(xml)
 {
 
+}
+
+bool XmltoSql::transfertEntries()
+{
+    bool ret = true;
+
+    auto accounts = m_xml.selectAccount();
+
+    for(auto account: accounts)
+    {
+        auto entriesXml = m_xml.selectEntry(account);
+        auto entriesDb = m_db.selectEntry(account);
+
+        QMap<int, Entry> map;
+
+        for(auto it: entriesDb)
+            map[it.id()] = it;
+
+        for(auto it: entriesXml)
+        {
+            if(map.contains(it.id()))
+                ret &= m_db.updateEntry(it);
+            else
+                ret &= m_db.addEntry(it);
+        }
+    }
+
+    return ret;
+}
+
+bool XmltoSql::transfertcategories()
+{
+    bool ret = false;
+
+    auto categoriesXml = m_xml.selectCategory();
+    auto categoriesDb = m_db.selectCategory();
+
+    for(auto it = categoriesXml.begin(); it != categoriesXml.end(); it++)
+            if(categoriesDb[it.key()] != it.value())
+                m_db.addCategory(it.key(), it.value());
+
+    return ret;
+
+}
+
+bool XmltoSql::exec()
+{
+    bool ret = true;
+
+    if(!m_db.isConnected())
+        return false;
+
+    ret = transfertEntries();
+    ret &= transfertcategories();
+
+    return ret;
 }
