d2f3003a3038caf21549aba8444422bbe1a964a8
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-02-08 16:16:33 +0100

    [FrequencyModel] add getter and setter, and generation from template

--- Account/Model/frequency.h
+++ Account/Model/frequency.h
@@ -8,12 +8,30 @@ class Frequency
 {
 private:
     int m_id;
-    AccountEnum::FrequencyEnum m_when;
+    AccountEnum::FrequencyEnum m_occurancy;
     Entry m_template;
     QMap<QDate, Entry> m_past;
 
+    int toDayNumber(AccountEnum::FrequencyEnum, QDate) const;
+
 public:
     Frequency();
+    Frequency(const Frequency&);
+    ~Frequency();
+
+    Frequency& operator=(const Frequency&);
+
+    QMap<QDate,Entry> getNew(QDate) const;
+    QMap<QDate, Entry> getPrev(QDate) const;
+
+    AccountEnum::FrequencyEnum occurancy() const;
+    void setOccurancy(AccountEnum::FrequencyEnum );
+    Entry getTemplate() const;
+    void setTemplate(Entry);
+    QMap<QDate, Entry> past() const;
+    void setPast(QMap<QDate, Entry>);
+    int id() const;
+    void setId(int id);
 };
 
 #endif // FREQUENCY_H
