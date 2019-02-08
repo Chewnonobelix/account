a86d03a9f6a965e971893e1499ce539d45547f17
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-02-08 12:03:54 +0100

    [Frequency] add attributes

--- Account/Model/accountglobal.h
+++ Account/Model/accountglobal.h
@@ -1,4 +1,14 @@
 #ifndef ACCOUNTGLOBAL_H
 #define ACCOUNTGLOBAL_H
 
+#include <QObject>
+class AccountEnum
+{
+    Q_GADGET
+
+public:
+    enum FrequencyEnum { never, daily, weekly, bimonthly, monthly, annualy};
+    Q_ENUM(FrequencyEnum)
+};
+
 #endif // ACCOUNTGLOBAL_H
