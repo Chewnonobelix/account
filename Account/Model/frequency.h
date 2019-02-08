5533e27d7215f7ee844588cd8e1f6c917061ed97
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-02-08 12:03:54 +0100

    [Frequency] add attributes

--- Account/Model/frequency.h
+++ Account/Model/frequency.h
@@ -1,9 +1,17 @@
 #ifndef FREQUENCY_H
 #define FREQUENCY_H
 
+#include "accountglobal.h"
+#include "entry.h"
 
 class Frequency
 {
+private:
+    int m_id;
+    AccountEnum::FrequencyEnum m_when;
+    Entry m_template;
+    QMap<QDate, Entry> m_past;
+
 public:
     Frequency();
 };
