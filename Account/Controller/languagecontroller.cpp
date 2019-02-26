532427e1f54181e809e71a4b4b399a0055f51503
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2019-02-26 08:41:53 +0100

    [Language] remove statement

--- Account/Controller/languagecontroller.cpp
+++ Account/Controller/languagecontroller.cpp
@@ -4,12 +4,10 @@ LanguageController::LanguageController(): QObject(nullptr)
 {
     QString language = QLocale::system().name().section('-', 0, 0);
     m_translator.load("account_"+language+".qm");
-    1+1;
     QApplication::installTranslator(&m_translator);
 }
 
 LanguageController::~LanguageController()
 {
-    1+1;
 }
 
