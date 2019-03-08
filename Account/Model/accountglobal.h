1bde77c519743cca97f3a813b0aa20ed855cceb6
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-03-08 16:26:19 +0100

    [AccountGlobal] frequency enum

--- Account/Model/accountglobal.h
+++ Account/Model/accountglobal.h
@@ -1,4 +1,14 @@
 #ifndef ACCOUNTGLOBAL_H
 #define ACCOUNTGLOBAL_H
 
+#include <QObject>
+
+class Account
+{
+    Q_GADGET
+public:
+    enum class FrequencyEnum {unique, day, week, month, quarter, year};
+    Q_ENUM(FrequencyEnum)
+};
+
 #endif // ACCOUNTGLOBAL_H
