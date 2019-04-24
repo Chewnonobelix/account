a9c85016efac7a7a9084b3fb6e52a429f9396de9
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-04-24 11:08:08 +0200

    [Budget] reset model class

--- Account/Model/budget.h
+++ Account/Model/budget.h
@@ -4,6 +4,8 @@
 //TODO v2
 #include <QObject>
 #include <QSet>
+#include <QMap>
+#include <QList>
 #include <QDebug>
 #include "total.h"
 #include "accountglobal.h"
@@ -14,12 +16,6 @@ public:
 
 private:
     int m_id;
-    Total m_current;
-    double m_target;
-    Account::FrequencyEnum m_frequency;
-    QDate m_start;
-    QSet<int> m_related;
-    QString m_category;
 
 public:
     Budget();
@@ -27,59 +23,7 @@ public:
 
     int id() const;
     void setId(int);
-    double target() const;
-    void setTarget(double);
-    Account::FrequencyEnum  frequency() const;
-    void setFrequency(Account::FrequencyEnum );
-    QDate start() const;
-    void setStart(QDate);
-    double current() const;
-
-    QDate next(QDate) const;
-    QDate prev(QDate) const;
-    QString category() const;
-    void setCategory(QString);
-    bool in(QDate) const;
-    friend Budget& operator <<(Budget&, Entry&);
-    friend Budget& operator >>(Budget&, Entry&);
     Budget& operator = (const Budget&);
-
-    Budget clone(QDate) const;
-
-};
-
-class OverBudget
-{
-private:
-    int m_id;
-    double m_target;
-    Account::FrequencyEnum m_frequency;
-    QDate m_start;
-    QSet<int> m_sub;
-    QString m_category;
-
-    OverBudget();
-    Budget createSub(QDate);
-
-public:
-    OverBudget(const OverBudget&);
-    ~OverBudget();
-
-    OverBudget& operator =(const OverBudget&);
-    bool add(Entry&);
-    bool remove(Entry&);
-    static OverBudget create(QString);
-
-    int id() const;
-    double target() const;
-    void setTarget(double);
-    Account::FrequencyEnum frequency() const;
-    void setFrequency(const Account::FrequencyEnum);
-    QDate start() const;
-    void setStart(QDate);
-    QString category() const;
-    void setCategory(QString);
-    void setId(int id);
 };
 
 #endif // BUDGET_H
