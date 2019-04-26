631dbaac08ce995930ca3d666dd32a35c882b7fb
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-04-26 16:49:45 +0200

    [SubBudget] bones

--- Account/Model/subbudget.cpp
+++ Account/Model/subbudget.cpp
@@ -4,3 +4,73 @@ SubBudget::SubBudget()
 {
 
 }
+
+SubBudget::SubBudget(const SubBudget&)
+{
+//TODO
+}
+
+SubBudget& SubBudget::operator = (const SubBudget&)
+{
+    //TODO
+    return *this;
+}
+
+double SubBudget::current() const
+{
+    //TODO
+    return 0;
+}
+
+double SubBudget::target() const
+{
+    //TODO
+    return 0;
+}
+
+void SubBudget::setTrget(double)
+{
+    //TODO
+}
+
+QDate SubBudget::begin() const
+{
+    //TODO
+    return QDate();
+}
+
+void SubBudget::setBegin(QDate)
+{
+    //TODO
+}
+
+QDate SubBudget::end() const
+{
+    //TODO
+    return QDate();
+
+}
+
+void SubBudget::setEnd(QDate)
+{
+    //TODO
+}
+
+bool SubBudget::addEntry(Entry)
+{
+    //TODO
+    return false;
+}
+
+bool SubBudget::removeEntry(Entry)
+{
+    //TODO
+    return false;
+}
+
+bool SubBudget::updateEntry(Entry)
+{
+    //TODO
+    return false;
+}
+
--- Account/Model/subbudget.h
+++ Account/Model/subbudget.h
@@ -1,11 +1,36 @@
 #ifndef SUBBUDGET_H
 #define SUBBUDGET_H
 
+#include <QMap>
+#include <QDate>
+#include "entry.h"
 
 class SubBudget
 {
+private:
+    double m_current;
+    double m_target;
+    QDate m_begin;
+    QDate m_end;
+    QMap<int, double> m_values;
+
 public:
     SubBudget();
+    SubBudget(const SubBudget&);
+
+    SubBudget& operator = (const SubBudget&);
+
+    double current() const;
+    double target() const;
+    void setTrget(double);
+    QDate begin() const;
+    void setBegin(QDate);
+    QDate end() const;
+    void setEnd(QDate);
+
+    bool addEntry(Entry);
+    bool removeEntry(Entry);
+    bool updateEntry(Entry);
 };
 
 #endif // SUBBUDGET_H
