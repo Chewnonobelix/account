f2b1e68bbd9b2e18ebc3f13fac0caef696a50bbc
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-11-28 16:24:34 +0100

    [ControllerCommon] display list of common

--- Account/Controller/controllercommon.cpp
+++ Account/Controller/controllercommon.cpp
@@ -2,11 +2,23 @@
 
 int ControllerCommon::exec()
 {
+    QVariantList model;
+    for(auto it: m_db->selectCommon())
+        model<<QVariant::fromValue(it);
+    
+    QObject* list = m_view->findChild<QObject*>("listCommon");
+
+    if(list)
+        list->setProperty("model", model);
+    
+    return 0;
+}
+
+void ControllerCommon::init()
+{
     QObject* checker = m_view->findChild<QObject*>("close");
     if(checker)
         connect(checker, SIGNAL(s_checked(bool)), this, SLOT(closeCommon(bool)));
-    
-    return 0;
 }
 
 void ControllerCommon::closeCommon(bool isClose)
