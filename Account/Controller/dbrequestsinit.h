2a59ea7aae84647bcdc866f64a63bab4b0deef13
Arnaud DUHAMEL <a.duhamel@ateme.com>
2020-01-08 14:59:30 +0100

    [SQL] sqlite database

--- Account/Controller/dbrequestsinit.h
+++ Account/Controller/dbrequestsinit.h
@@ -4,83 +4,79 @@
 #include <QLatin1String>
 
 const auto account_table = QString("CREATE TABLE `account` ("
-                                       "`id` int(11) NOT NULL,"
-                                       "`account` varchar(50) NOT NULL,"
+                                       "`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                                       "`account` TEXT NOT NULL,"
                                        "`value` double NOT NULL,"
                                        "`date_eff` date DEFAULT NULL,"
-                                       "`type` varchar(200) NOT NULL  DEFAULT `outcome` ,"
-                                       "`profile` varchar(200) NOT NULL DEFAULT `Default`,"
-                                       "`commonExpanse` int(11) DEFAULT NULL,"
-                                       "`frequencyReference` int(11) DEFAULT NULL)");
+                                       "`type` TEXT NOT NULL  DEFAULT `outcome` ,"
+                                       "`profile` TEXT NOT NULL DEFAULT `Default`,"
+                                       "`commonExpanse` INTEGER DEFAULT NULL,"
+                                       "`frequencyReference` INTEGER DEFAULT NULL)");
 
-const auto budget_table = QString("CREATE TABLE `budget` ("
-                                      "`id` int(11) NOT NULL,"
-                                      "`category` int(11) NOT NULL,"
-                                      "`reference` date NOT NULL,"
-                                      "`profile` varchar(200) NOT NULL DEFAULT 'Default',"
-                                      "`account` varchar(200) NOT NULL)");
+const auto entrymetadata_table = QString("CREATE TABLE `entrymetadata` ("
+                                             "`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                                             "`entry` INTEGER NOT NULL REFERENCES account(id), "
+                                             "`name` TEXT NOT NULL, "
+                                             "`value` TEXT NOT NULL, "
+                                             "CONSTRAINT meta_unique UNIQUE(entry, name)"
+                                           ")");
 
 const auto categories_table = QString("CREATE TABLE `categories` ("
-                                          "`ID` int(11) NOT NULL,"
-                                          "`name` varchar(50) NOT NULL,"
-                                          "`type` varchar(200) DEFAULT NOT NULL DEFAULT `outcome`,"
-                                          "`account` varchar(200) NOT NULL,"
-                                          "`profile` varchar(200) NOT NULL)");
-
-const auto commonEntry_table = QString("CREATE TABLE `commonEntry` ("
-                                           "`idCommon` int(11) NOT NULL,"
-                                           "`name` varchar(200) NOT NULL,"
-                                           "`entry` int(11) NOT NULL"
-                                         ")");
-
-const auto commonExpanse_table = QString("CREATE TABLE `commonExpanse` ("
-                                             "`id` int(11) NOT NULL,"
-                                             "`begin` date NOT NULL,"
-                                             "`isClose` tinyint(1) NOT NULL DEFAULT '0',"
-                                             "`title` varchar(200) NOT NULL,"
-                                             "`profile` varchar(200) NOT NULL DEFAULT 'Default',"
-                                             "`account` varchar(200) NOT NULL"
-                                           ")");
-
-const auto entrymetadata_table = QString("CREATE TABLE `entrymetada` ("
-                                             "`id` int(11) NOT NULL,"
-                                             "`entry` int(11) NOT NULL,"
-                                             "`name` varchar(200) NOT NULL,"
-                                             "`value` varchar(200) NOT NULL"
-                                           ")");
-
-const auto frequency_table = QString("CREATE TABLE `frequency` ("
-                                         "`id` int(11) NOT NULL,"
-                                         "`freq` int(11) NOT NULL,"
-                                         "`nbGroup` int(11) NOT NULL,"
-                                         "`end` date DEFAULT NULL,"
-                                         "`profile` varchar(200) NOT NULL DEFAULT 'Default',"
-                                         "`account` varchar(200) NOT NULL"
-                                       ")");
+                                          "`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                                          "`name` TEXT NOT NULL,"
+                                          "`type` TEXT NOT NULL DEFAULT `outcome`,"
+                                          "`account` TEXT NOT NULL,"
+                                          "`profile` TEXT NOT NULL,"
+                                      "CONSTRAINT category_unique UNIQUE(name, account, profile))");
 
 const auto information_table = QString("CREATE TABLE `information` ("
-                                           "`id` int(11) NOT NULL,"
-                                           "`id_entry` int(11) NOT NULL,"
-                                           "`info` varchar(200) NOT NULL,"
-                                           "`prev` tinyint(1) NOT NULL DEFAULT '0',"
-                                           "`category` int(11) DEFAULT NULL"
+                                           "`id` INTEGER NOT NULL  PRIMARY KEY AUTOINCREMENT,"
+                                           "`idEntry` INTEGER NOT NULL REFERENCES account(id),"
+                                           "`info` TEXT NOT NULL,"
+                                           "`prev` INTEGER NOT NULL DEFAULT '0',"
+                                           "`category` INTEGER DEFAULT NULL REFERENCES categories(id)"
                                          ")");
 
+const auto budget_table = QString("CREATE TABLE `budget` ("
+                                      "`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                                      "`category` INTEGER NOT NULL REFERENCES categories(id),"
+                                      "`reference` date NOT NULL,"
+                                      "`profile` TEXT NOT NULL DEFAULT 'Default',"
+                                      "`account` TEXT NOT NULL)");
+
 const auto subbudget_table = QString("CREATE TABLE `subbudget` ("
-                                         "`ID` int(11) NOT NULL,"
-                                         "`idBudget` int(11) NOT NULL,"
-                                         "`frequency` int(11) NOT NULL,"
-                                         "`target` double NOT NULL,"
+                                         "`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                                         "`idBudget` INTEGER NOT NULL REFERENCES budget(id),"
+                                         "`frequency` INTEGER NOT NULL,"
+                                         "`target` REAL NOT NULL,"
                                          "`fromDate` date NOT NULL"
                                        ")");
 
-const auto account_key = QString("ALTER TABLE `account`"
-                                 "ADD PRIMARY KEY (`id`),"
-                                 "ADD KEY `type` (`type`)"
-                                 "MODIFY `id` int(11) NOT NULL AUTO_INCREMENT");
+const auto frequency_table = QString("CREATE TABLE `frequency` ("
+                                         "`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                                         "`freq` INTEGER NOT NULL,"
+                                         "`nbGroup` INTEGER NOT NULL,"
+                                         "`end` date DEFAULT NULL,"
+                                         "`profile` TEXT NOT NULL DEFAULT 'Default',"
+                                         "`account` TEXT NOT NULL"
+                                       ")");
 
-const auto information_key = QString("ALTER TABLE `information`"
-                                     "ADD PRIMARY KEY (`id`)"
-                                     "MODIFY `id` int(11) NOT NULL AUTO_INCREMENT");
+const auto commonExpanse_table = QString("CREATE TABLE `commonExpanse` ("
+                                             "`id` INTEGER NOT NULL  PRIMARY KEY AUTOINCREMENT,"
+                                             "`begin` date NOT NULL,"
+                                             "`isClose` tinyint(1) NOT NULL DEFAULT '0',"
+                                             "`title` TEXT NOT NULL,"
+                                             "`profile` TEXT NOT NULL DEFAULT 'Default',"
+                                             "`account` TEXT NOT NULL"
+                                           ")");
+
+const auto commonEntry_table = QString("CREATE TABLE `commonEntry` ("
+                                           "`idCommon` INTEGER NOT NULL REFERENCES commonExpanse (id),"
+                                           "`name` TEXT NOT NULL,"
+                                           "`entry` INTEGER NOT NULL REFERENCES account(id)"
+                                         ")");
+
+
+
 
 #endif // DBREQUESTSINIT_H
