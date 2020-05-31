d2ebbc954213fa55b30b2440374c6cee8f1fe7e9
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2020-05-31 23:48:26 +0200

    [AG] change granularity

--- Account/Controller/abstractgraphcontroller.cpp
+++ Account/Controller/abstractgraphcontroller.cpp
@@ -1,6 +1,35 @@
 #include "abstractgraphcontroller.h"
 
-AbstractGraphController::AbstractGraphController()
+int AbstractGraphController::exec()
+{
+    auto entries = m_db->selectEntry(currentAccount());
+
+
+    return 0;
+}
+
+void AbstractGraphController::change(int way)
+{
+    switch (m_currentGran)
+    {
+    case Account::Month:
+        m_currentGran = way > 0 ? Account::Year : Account::Over;
+        break;
+    case Account::Year:
+        m_currentGran = way > 0 ? Account::Over : Account::Month;
+        break;
+    case Account::Over:
+        m_currentGran = way > 0 ? Account::Month : Account::Year;
+        break;
+    }
+
+    for(auto it: m_graphList)
+        it->setGran(m_currentGran);
+
+    increment();
+}
+
+void AbstractGraphController::increment(int)
 {
 
 }
--- Account/Controller/abstractgraphcontroller.h
+++ Account/Controller/abstractgraphcontroller.h
@@ -1,12 +1,25 @@
 #ifndef ABSTRACTGRAPHCONTROLLER_H
 #define ABSTRACTGRAPHCONTROLLER_H
 
+#include "abstractcontroller.h"
+#include "InterfaceGraph.h"
 
 class AbstractGraphController : public AbstractController
 {
     Q_OBJECT
+private:
+    Account::Granularity m_currentGran = Account::Month;
+    QDate m_currentDate = QDate::currentDate();
+    QList<QSharedPointer<InterfaceGraph>> m_graphList;
+
 public:
-    AbstractGraphController();
+    AbstractGraphController() = default;
+    ~AbstractGraphController() = default;
+
+public slots:
+    void change(int);
+    void increment(int = 0);
+    int exec() override;
 };
 
 #endif // ABSTRACTGRAPHCONTROLLER_H
