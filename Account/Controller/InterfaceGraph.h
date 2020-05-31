63245e85d1bd0b860b7987d7cc040c57b78fbb1b
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2020-05-31 23:48:01 +0200

    [IG] set date and gran

--- Account/Controller/InterfaceGraph.h
+++ Account/Controller/InterfaceGraph.h
@@ -2,14 +2,15 @@
 #define INTERFACEGRAPH_H
 
 #include "Model/entry.h"
+#include "Model/accountglobal.h"
 
 class InterfaceGraph
 {
 public:
-    virtual void change(int) = 0;
-    virtual void increment(int = 0) = 0;
     virtual void add(const Entry&) = 0;
     virtual void clear() = 0;
+    virtual void setDate(QDate) = 0;
+    virtual void setGran(Account::Granularity) = 0;
 };
 
 #endif // INTERFACEGRAPH_H
