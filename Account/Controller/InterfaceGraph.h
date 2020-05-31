0dfbb05601cced81dc481720b0a1f8a47775905a
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2020-05-31 23:25:55 +0200

    [Interface] graph interface

--- Account/Controller/InterfaceGraph.h
+++ Account/Controller/InterfaceGraph.h
@@ -1,4 +1,15 @@
 #ifndef INTERFACEGRAPH_H
 #define INTERFACEGRAPH_H
 
+#include "Model/entry.h"
+
+class InterfaceGraph
+{
+public:
+    virtual void change(int) = 0;
+    virtual void increment(int = 0) = 0;
+    virtual void add(const Entry&) = 0;
+    virtual void clear() = 0;
+};
+
 #endif // INTERFACEGRAPH_H
