8439a531c1b4cfc6a8bf3b7193500e3dd2845cf5
Arnaud duhamel <duhamel@abvent.fr>
2020-09-04 16:17:39 +0200

    [Debt] add controller and model class

---
+++ Account/Controller/features/controllerdebt.h
@@ -0,0 +1,12 @@
+#ifndef CONTROLLERDEBT_H
+#define CONTROLLERDEBT_H
+
+#include <QtCore/qglobal.h>
+
+class ControllerDebt
+{
+public:
+    ControllerDebt();
+};
+
+#endif // CONTROLLERDEBT_H
---
+++ Account/Model/debt.h
@@ -0,0 +1,12 @@
+#ifndef DEBT_H
+#define DEBT_H
+
+#include <QtCore/qglobal.h>
+
+class Debt
+{
+public:
+    Debt();
+};
+
+#endif // DEBT_H
