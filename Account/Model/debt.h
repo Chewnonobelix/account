b728cbc092d3f1a84c139dd4f4bc18118b6b8757
Arnaud duhamel <duhamel@abvent.fr>
2020-09-04 16:37:32 +0200

    [Debt] struct

--- Account/Model/debt.h
+++ Account/Model/debt.h
@@ -1,12 +1,34 @@
 #ifndef DEBT_H
 #define DEBT_H
 
+#include "../account_global.h"
+#include "accountglobal.h"
+#include "entry.h"
+#include "frequency.h"
+#include "metadata.h"
 #include <QtCore/qglobal.h>
 
-class Debt
+class ACCOUNT_EXPORT Debt : public MetaData
 {
 public:
-    Debt();
+    Debt() = default;
+    Debt(const Debt &) = default;
+    ~Debt() = default;
+
+    Debt &operator=(const Debt &) = default;
+
+    QUuid id() const;
+    void setId(QUuid);
+    Entry initial() const;
+    void setInitial(Entry);
+    Frequency scheduler() const;
+    void setScheduler(Frequency);
+    double rate() const;
+    void setRate(double);
+    int nb() const;
+    void setNb(int);
+    Entry last() const;
+    void setLast(Entry);
 };
 
 #endif // DEBT_H
