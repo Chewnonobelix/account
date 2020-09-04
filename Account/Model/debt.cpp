4c82644a390f7e030448e7fd88ca284605543ea5
Arnaud duhamel <duhamel@abvent.fr>
2020-09-04 16:37:32 +0200

    [Debt] struct

--- Account/Model/debt.cpp
+++ Account/Model/debt.cpp
@@ -1,6 +1,43 @@
 #include "debt.h"
 
-Debt::Debt()
+QUuid Debt::id() const
 {
-
+    return QUuid();
 }
+
+void Debt::setId(QUuid) {}
+
+Entry Debt::initial() const
+{
+    return Entry();
+}
+
+void Debt::setInitial(Entry) {}
+
+Frequency Debt::scheduler() const
+{
+    return Frequency();
+}
+
+void Debt::setScheduler(Frequency) {}
+
+double Debt::rate() const
+{
+    return 0.0;
+}
+
+void Debt::setRate(double) {}
+
+int Debt::nb() const
+{
+    return 0;
+}
+
+void Debt::setNb(int) {}
+
+Entry Debt::last() const
+{
+    return Entry();
+}
+
+void Debt::setLast(Entry) {}
