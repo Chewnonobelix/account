a0e941ca1ebd595632fbcc9c7412998d40412e5f
Arnaud duhamel <duhamel@abvent.fr>
2020-10-09 17:38:12 +0200

    [Category] change category

--- Account/Controller/core/controllerinformation.cpp
+++ Account/Controller/core/controllerinformation.cpp
@@ -2,92 +2,96 @@
 
 int ControllerInformation::exec()
 {
-    if (m_entry.hasMetadata("frequency")) {
-        emit frequencyVisible(m_entry.hasMetadata("frequency"));
+ if (m_entry.hasMetadata("frequency")) {
+  emit frequencyVisible(m_entry.hasMetadata("frequency"));
 
-        if (m_entry.hasMetadata("frequency")) {
-            QUuid f = m_entry.metaData<QUuid>("frequency");
-            int g = m_entry.metaData<int>("freqGroup");
-            auto freqs = m_db->selectFrequency();
-            int page = m_currentPage;
-            page--;
-            for(auto it: freqs)
-            {
-                if(it.id() == f)
-                {
-                    auto le = it.listEntries(g);
-                    QVariantList model;
-                    emit maxPageChanged(le.size() / 100 + 1);
-                    emit currentPageChanged(1);
-                    for (int i = (page * 100); i < le.size() && i < (page + 1) * 100; i++)
-                        model << le[i];
+  if (m_entry.hasMetadata("frequency")) {
+   QUuid f = m_entry.metaData<QUuid>("frequency");
+   int g = m_entry.metaData<int>("freqGroup");
+   auto freqs = m_db->selectFrequency();
+   int page = m_currentPage;
+   page--;
+   for (auto it : freqs) {
+	if (it.id() == f) {
+	 auto le = it.listEntries(g);
+	 QVariantList model;
+	 emit maxPageChanged(le.size() / 100 + 1);
+	 emit currentPageChanged(1);
+	 for (int i = (page * 100); i < le.size() && i < (page + 1) * 100; i++)
+	  model << le[i];
 
-                    emit pageModel(QVariant::fromValue(model));
-                }
-            }
-        }
-    }
+	 emit pageModel(QVariant::fromValue(model));
+	}
+   }
+  }
+ }
 
-    return 0;
+ return 0;
 }
 
 void ControllerInformation::view(QUuid id)
 {
-    m_entry = AbstractController::entry(id);
+ m_entry = AbstractController::entry(id);
 
-    QStringList catList /*= m_db->selectCategory().values(m_entry.type())*/;
-    catList << "";
-    emit this->catList(catList);
+ QStringList catList /*= m_db->selectCategory().values(m_entry.type())*/;
+ catList << "";
+ emit this->catList(catList);
 
-    emit view(QVariant::fromValue(m_entry));
+ emit view(QVariant::fromValue(m_entry));
 
-    exec();
+ exec();
 }
 
 void ControllerInformation::titleChange(QString title)
 {
-    m_entry.setTitle(title);
+ m_entry.setTitle(title);
 
-    updateEntry(m_entry);
+ updateEntry(m_entry);
 }
 
 void ControllerInformation::valueChange(double value)
 {
-    m_entry.setValue(value);
+ m_entry.setValue(value);
 
-    updateEntry(m_entry);
-    emit s_update(m_entry.id());
+ updateEntry(m_entry);
+ emit s_update(m_entry.id());
 }
 
 void ControllerInformation::categoryChange(QString cat)
 {
-    //    QString old = m_entry.category();
-    //    m_entry.setCategory(cat);
+ auto list = m_db->selectCategory()[m_entry.type()];
+ Category c;
+ auto i = std::find_if(list.begin(), list.end(), [cat](Category it) { return it.name() == cat; });
 
-    //    updateEntry(m_entry);
-    //    emit s_changeCat(old, m_entry.id());
+ c = *i;
+ qDebug() << cat << c;
+ //    QString old = m_entry.category();
+ m_entry.setCategory(c);
+
+ updateEntry(m_entry);
+ //    emit s_changeCat(old, m_entry.id());
 }
 
 void ControllerInformation::addNewCategory(QString cat)
 {
-    Category c;
-    c.setName(cat);
-    c.setType(m_entry.type());
-    //    qDebug() << "cat" << cat << m_db->addCategory(c);
+ Category c;
+ c.setName(cat);
+ c.setType(m_entry.type());
+ m_db->addCategory(c);
 
-    //    categoryChange(c);
-    view(m_entry.id());
-    emit s_exec();
+ categoryChange(cat);
+ view(m_entry.id());
+ emit s_exec();
 }
 
 void ControllerInformation::pageChange()
 {
-    exec();
+ exec();
 }
 
 void ControllerInformation::supportChange(int support)
 {
-    m_entry.setSupport((Account::SupportEnum)support);
-    m_db->updateEntry(m_entry);
-    emit s_exec();
+ m_entry.setSupport((Account::SupportEnum) support);
+ m_db->updateEntry(m_entry);
+ emit s_exec();
 }
--- Account/View/Core/CategoryItem.qml
+++ Account/View/Core/CategoryItem.qml
@@ -16,7 +16,8 @@ Account*/ComboBox {
     }
 
     property int type: Account.Outcome
-
+    signal addCategory(string cat)
+    property bool blocked: false
 
     model: _categoryModel
 
@@ -41,104 +42,4 @@ Account*/ComboBox {
             cursorShape: Qt.PointingHandCursor
         }
     }
-
-
-//        property Rectangle back2: Rectangle{
-
-//        }
-
-//        contentItem: AccountTextInput {
-//                background: root.editable ? back2 : back
-//                readOnly: !root.editable
-//                text: categoryDelegate.text
-//                z:10
-//                onAccepted:  {
-//                    if(!readOnly) {
-//                        root.addCategory(text)
-//                    }
-//                }
-//        }
-//    }
-
-    signal addCategory(string cat)
-    signal currentTextChanged(string cat)
-    property bool blocked: false
-
-//    function close() {
-//        currentTextChanged(currentText)
-//    }
-
-//    Connections {
-//        target: popup
-
-//        function onClosed() {
-//            currentTextChanged(currentText)
-//        }
-//    }
-    
-//    function setting(type) {
-//        var index = find(type)
-
-//        return index >= 0 ? index : model.lenth - 1
-//    }
-
-//    contentItem: AccountTextInput {
-//        id: input
-//        horizontalAlignment: Qt.AlignHCenter
-//        verticalAlignment: Qt.AlignVCenter
-
-//        ToolTip.text: readOnly ? qsTr("Select a transaction category") : qsTr("Add a new category")
-
-//        property Item back2: AccountLabel{
-//            text: input.text
-//        }
-
-//        background: readOnly ? back2 : back
-//        text: currentText
-//        readOnly: !editable
-//        onAccepted: {
-//            addCategory(text)
-//        }
-//        MouseArea {
-//            anchors.fill: parent
-//            propagateComposedEvents: true
-//            acceptedButtons: editable ? Qt.NoButton : Qt.LeftButton
-//            onClicked: popup.open()
-//            cursorShape: editable ? Qt.IBeamCursor : Qt.PointingHandCursor
-//        }
-//    }
-    
-    
-//    editable: currentText === ""
-//    delegate: ItemDelegate {
-//        width: root.width
-//        contentItem: Rectangle  {
-//            gradient: AccountStyle.goldButton
-//            anchors.fill: parent
-
-//            AccountLabel {
-//                color: "black"
-//                text: modelData
-//                anchors.centerIn: parent
-//            }
-
-//            MouseArea {
-//                property string cTxt: modelData
-//                anchors.fill: parent
-//                cursorShape: index === (root.count - 1)  ? Qt.WhatsThisCursor : Qt.PointingHandCursor
-
-//                ToolTip.visible: (index === (root.count - 1)) && hovered
-//                ToolTip.text: qsTr("Add a new category")
-//                ToolTip.delay: 500
-
-//                onClicked: {
-//                    if(mouse.button === Qt.LeftButton) {
-//                        var index = root.find(cTxt)
-//                        root.currentIndex = index
-//                        root.popup.close()
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
