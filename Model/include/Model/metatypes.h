dcaec19077f30dd400b428aad6a4d5058b2abdee
Chewnonobelix <arnaud.duhamel.pro@gmail.com>
2026-08-01 20:09:37 +0200

    feat(Model): Introduce metatypes.h for centralized meta-type declarations

---
+++ Model/include/Model/metatypes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "enums.h"
+
+#include <QDate>
+#include <QList>
+#include <QUuid>
+
+Q_DECLARE_METATYPE(QList<QUuid>)
+Q_DECLARE_METATYPE(QList<QDate>)
+Q_DECLARE_METATYPE(QList<OpenAccountEnums::Support>)
